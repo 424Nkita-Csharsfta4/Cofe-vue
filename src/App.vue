3206c2f428555fd64551907548e52b96f3d1d675
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-11-01 11:31:14 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,452 @@
+<template>
+  
+ 
+
+   <header class="header">
+   <div class="navbar-area">
+   <div class="container">
+   <div class="row align-items-center">
+   <div class="col-lg-12">
+   <nav class="navbar navbar-expand-lg">
+   <a class="navbar-brand" href="#">
+   <img src="./assets/Coffeeouse.svg" alt="Logo" />
+   </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="toggler-icon"></span>
+   <span class="toggler-icon"></span>
+   <span class="toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+   <ul id="nav" class="navbar-nav ml-auto">
+   <li class="nav-item">
+   <a class="page-scroll" href="#home">Home</a>
+   </li>
+   <li class="nav-item">
+   <a class="page-scroll" href="#services">Why us</a>
+   </li>
+   <li class="nav-item">
+   <a class="page-scroll" href="#about">Menu</a>
+   </li>
+   <li class="nav-item">
+   <a class="page-scroll" href="#how">Our locations</a>
+   </li>
+   <li class="nav-item">
+   <a class="page-scroll" href="#testimonial">Newsletter</a>
+   </li>
+   </ul>
+   </div>
+   </nav>
+   </div>
+   </div>
+   </div>
+   </div>
+   </header>
+   <section id="home" class="hero-section">
+   <div class="hero-shape">
+   <img src="./assets/hero.jpg" alt="" class="shape" />
+   </div>
+   <div class="container">
+   <div class="row align-items-center">
+   <div class="col-lg-6">
+   <div class="hero-content">
+   <h1 class="wow fadeInUp" data-wow-delay=".2s">The Coffee<span> Roasters</span> </h1>
+   <p class="wow fadeInUp" data-wow-delay=".4s">
+    We source our coffee seasonally, from farmers who care, and roast to highlight what makes them special.</p>
+   <a href="javascript:void(0)" class="main-btn btn-hover wow fadeInUp" data-wow-delay=".6s">View our menu</a>
+   </div>
+   </div>
+   <div class="col-lg-6">
+   <div class="hero-img wow fadeInUp" data-wow-delay=".5s">
+   <img src="./assets/cta.jpg" alt="" />
+   </div>
+   </div>
+   </div>
+   </div>
+   </section>
+   <section id="services" class="service-section pt-150">
+   <div class="container">
+   <div class="row justify-content-center">
+   <div class="col-xl-6 col-lg-8">
+   <div class="section-title text-center mb-70">
+   <span class="wow fadeInUp" data-wow-delay=".2s">Explore our menu</span>
+   <h1 class="wow fadeInUp" data-wow-delay=".4s">Available all day! We can not guarantee our foods are allergen-free. Service charge not included.</h1>
+   </div>
+   </div>
+   </div>
+   <div class="row">
+   <div class="col-lg-4 col-md-6">
+   <div class="single-service wow fadeInUp" data-wow-delay=".2s">
+   <div class="icon">
+   <img src="./assets/1.svg" alt="" />
+   </div>
+   <div class="content">
+   <h3>Prufrock Breakfast Bun</h3>
+   <p>Brioche bun, with bacon, scrambled egg, melty cheese and mustard</p>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-service wow fadeInUp" data-wow-delay=".4s">
+   <div class="icon">
+   <img src="./assets/2.svg" alt="" />
+   </div>
+   <div class="content">
+   <h3>Toast (Vegan on request)</h3>
+   <p>Sourdough toast with butter and blueberry and blackcurrant jam</p>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-service wow fadeInUp" data-wow-delay=".6s">
+   <div class="icon">
+   <img src="./assets/3.svg" alt="" />
+   </div>
+   <div class="content">
+   <h3>Mushrooms (Vegan on request)</h3>
+   <p>Field mushrooms on toast with graceburn cheese and aioli</p>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-service wow fadeInUp" data-wow-delay=".2s">
+   <div class="icon">
+   <img src="./assets/4.svg" alt="" />
+   </div>
+   <div class="content">
+   <h3>Eggs on toast</h3>
+   <p>Cackleberry eggs - poached, fried or scrambled on sourdough toast</p>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-service wow fadeInUp" data-wow-delay=".4s">
+   <div class="icon">
+   <img src="./assets/5.svg" alt="" />
+   </div>
+   <div class="content">
+   <h3>Soup (Vegan on request)</h3>
+   <p>Gazpacho served with sourdough toast</p>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-service wow fadeInUp" data-wow-delay=".6s">
+   <div class="icon">
+   <img src="./assets/1.svg" alt="" />
+   </div>
+   <div class="content">
+   <h3>Prufrock benny</h3>
+   <p>Cackleberry Farm poached eggs, Red Brick espresso hollandaise on toast</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </section>
+   <section id="about" class="about-section pt-150">
+   <div class="container">
+   <div class="row">
+   <div class="col-lg-6">
+   <div class="about-img wow fadeInUp" data-wow-delay=".5s">
+   <img src="./assets/2.svg" alt="" />
+   </div>
+   </div>
+   <div class="col-lg-6">
+   <div class="about-content">
+   <div class="section-title">
+   <span class="wow fadeInUp" data-wow-delay=".2s">Testimonials</span>
+   <h1 class="wow fadeInUp" data-wow-delay=".4s">I had a cup of iced coffee, which was perfect size for me. Not too big. The cup is stylish with slim design. The iced coffee was perfect for a warm day like today. 
+The cafe is clean and welcoming.</h1>
+   <p class="wow fadeInUp" data-wow-delay=".6s">Chiho Sharp, local expert</p>
+   </div>
+   <div class="rating-meta d-flex align-items-center wow fadeInUp" data-wow-delay=".65s">
+   <h5>Rating 4.8</h5>
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   </div>
+   <div class="counter-up wow fadeInUp" data-wow-delay=".8s">
+   <div class="single-counter">
+   <h3 id="secondo1" class="countup" >2K</h3>
+   <h5>Delyvery</h5>
+   </div>
+   <div class="single-counter position-relative">
+   <h3 id="secondo2" class="countup">2K </h3>
+   <h5>Happy User</h5>
+   </div>
+   <div class="single-counter">
+   <h3 id="secondo3" class="countup" >1k </h3>
+   <h5>Reviews</h5>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </section>
+   <section id="how" class="delivery-section pt-150">
+   <div class="container">
+   <div class="row align-items-center">
+   <div class="col-lg-5">
+   <div class="delivery-content">
+   <div class="section-title">
+   <span class="wow fadeInUp" data-wow-delay=".2s">Fast Delivery</span>
+   <h1 class="mb-35 wow fadeInUp" data-wow-delay=".4s">Order Now, Recieve Within 30mins</h1>
+   <p class="mb-35 wow fadeInUp" data-wow-delay=".6s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore hdht dolore magna aliquyam erat, sed diam voluptua.</p>
+   <a href="javscript:void(0)" class="main-btn btn-hover wow fadeInUp" data-wow-delay=".8s">Reviews</a>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-7 order-first order-lg-last">
+   <div class="delivery-img wow fadeInUp" data-wow-delay=".5s">
+   <img src="./assets/4.svg" alt="" />
+   </div>
+   </div>
+   </div>
+   </div>
+   </section>
+   <section id="received" class="about-section received-section pt-150">
+   <div class="container">
+   <div class="row align-items-center">
+   <div class="col-lg-6">
+   <div class="about-img received-img wow fadeInUp" data-wow-delay=".5s">
+   <img src="./assets/3.svg" alt="" />
+   </div>
+   </div>
+   <div class="col-lg-6">
+   <div class="about-content received-content">
+   <div class="section-title">
+   <span class="wow fadeInUp" data-wow-delay=".2s">Contactless Delivery</span>
+   <h1 class="mb-25 wow fadeInUp" data-wow-delay=".4s">On-time Delivery to Your Doorstep</h1>
+   <p class="wow fadeInUp" data-wow-delay=".6s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </section>
+   <section id="testimonial" class="testimonial-section img-bg pt-150 pb-40">
+   <div class="container">
+   <div class="row justify-content-center">
+   <div class="col-lg-6">
+   <div class="section-title mb-60 text-center">
+   <span class="wow fadeInUp" data-wow-delay=".2s">News</span>
+   <h1 class="wow fadeInUp" data-wow-delay=".4s">Stay updated on all the latest Coffee House products, events & community news.</h1>
+   </div>
+   </div>
+   </div>
+   <div class="row testimonial-wrapper">
+   <div class="col-lg-4 col-md-6 -mt-30">
+   <div class="single-testimonial wow fadeInUp" data-wow-delay=".2s">
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   <div class="content">
+   <p>Lorem ipsum dolor sit amet onsetetur sadipscing elitr, sed diam non eirmod tempor invidunt ut labore etdo magna aliquyam erat, sed diam vero eos et accusam et justo duo dolores et ea rebum clita kasd gubergren.</p>
+   </div>
+   <div class="info">
+   <div class="image">
+   <img src="https://cdn-icons-png.flaticon.com/512/219/219988.png" alt="" />
+   </div>
+   <div class="text">
+   <h5>Ena Shah</h5>
+   <p>Teacher at Abc School</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6 -mt-60">
+   <div class="single-testimonial wow fadeInUp" data-wow-delay=".4s">
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   <div class="content">
+   <p>Lorem ipsum dolor sit amet onsetetur sadipscing elitr, sed diam non eirmod tempor invidunt ut labore etdo magna aliquyam erat, sed diam vero eos et accusam et justo duo dolores et ea rebum clita kasd gubergren.</p>
+   </div>
+   <div class="info">
+   <div class="image">
+   <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png" alt="" />
+   </div>
+   <div class="text">
+   <h5>Mrs. Gosh</h5>
+   <p>Actor</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-testimonial wow fadeInUp" data-wow-delay=".6s">
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   <div class="content">
+   <p>Lorem ipsum dolor sit amet onsetetur sadipscing elitr, sed diam non eirmod tempor invidunt ut labore etdo magna aliquyam erat, sed diam vero eos et accusam et justo duo dolores et ea rebum clita kasd gubergren.</p>
+   </div>
+   <div class="info">
+   <div class="image">
+   <img src="https://cdn-icons-png.flaticon.com/512/219/219988.png" alt="" />
+   </div>
+   <div class="text">
+   <h5>John Doe</h5>
+   <p>Model</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6 -mt-30">
+   <div class="single-testimonial wow fadeInUp" data-wow-delay=".2s">
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   <div class="content">
+   <p>Lorem ipsum dolor sit amet onsetetur sadipscing elitr, sed diam non eirmod tempor invidunt ut labore etdo magna aliquyam erat, sed diam vero eos et accusam et justo duo dolores et ea rebum clita kasd gubergren.</p>
+   </div>
+   <div class="info">
+   <div class="image">
+   <img src="https://cdn-icons-png.flaticon.com/512/219/219988.png" alt="" />
+   </div>
+   <div class="text">
+   <h5>Jonathan Smith</h5>
+   <p>Creative Designer</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6 -mt-60">
+   <div class="single-testimonial wow fadeInUp" data-wow-delay=".4s">
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   <div class="content">
+   <p>Lorem ipsum dolor sit amet onsetetur sadipscing elitr, sed diam non eirmod tempor invidunt ut labore etdo magna aliquyam erat, sed diam vero eos et accusam et justo duo dolores et ea rebum clita kasd gubergren.</p>
+   </div>
+   <div class="info">
+   <div class="image">
+   <img src="https://cdn-icons-png.flaticon.com/512/219/219988.png" alt="" />
+   </div>
+   <div class="text">
+   <h5>Sara A. K.</h5>
+   <p>Heroine</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-4 col-md-6">
+   <div class="single-testimonial wow fadeInUp" data-wow-delay=".6s">
+   <div class="rating">
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   <i class="lni lni-star-filled"></i>
+   </div>
+   <div class="content">
+   <p>Lorem ipsum dolor sit amet onsetetur sadipscing elitr, sed diam non eirmod tempor invidunt ut labore etdo magna aliquyam erat, sed diam vero eos et accusam et justo duo dolores et ea rebum clita kasd gubergren.</p>
+   </div>
+   <div class="info">
+   <div class="image">
+   <img src="https://cdn-icons-png.flaticon.com/512/219/219988.png" alt="" />
+   </div>
+   <div class="text">
+   <h5>David Smith</h5>
+   <p>Businessman</p>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </section>
+   
+   <footer id="footer" class="footer pt-100 pb-70">
+   <div class="footer-shape">
+   <img src="./assets/4.svg" alt="" class="shape shape-1" />
+   <img src="./assets/5.svg" alt="" class="shape shape-2" />
+   </div>
+   <div class="container">
+   <div class="row">
+   <div class="col-lg-3 col-md-6">
+   <div class="footer-widget wow fadeInUp" data-wow-delay=".2s">
+   <div class="logo">
+   <a href="#"><img src="./assets/3.svg" alt="logo" /></a>
+   </div>
+   <div class="download-btns">
+  
+   </div>
+   </div>
+   </div>
+   <div class="col-lg-3 col-md-6">
+   <div class="footer-widget wow fadeInUp" data-wow-delay=".4s">
+   <h3>About Us</h3>
+   <ul class="links">
+   <li>
+   <a href="javascript:void(0)">Home</a>
+   </li>
+   <li>
+   <a href="javascript:void(0)">Services</a>
+   </li>
+   <li>
+   <a href="javascript:void(0)">About Us</a>
+   </li>
+   <li>
+   <a href="javascript:void(0)">Contact</a>
+   </li>
+   </ul>
+   </div>
+   </div>
+ 
+   <div class="col-lg-3 col-md-6">
+   <div class="footer-widget wow fadeInUp" data-wow-delay=".8s">
+   <h3>Support</h3>
+   <ul class="links">
+   <li>
+   <a href="javascript:void(0)">Open Ticket</a>
+   </li>
+   <li>
+   <a href="javascript:void(0)">Community</a>
+   </li>
+   <li>
+   <a href="javascript:void(0)">Return Policy</a>
+   </li>
+   <li>
+   <a href="javascript:void(0)">Accessibility</a>
+   </li>
+   </ul>
+   </div>
+   </div>
+   </div>
+   </div>
+   </footer>
+   <a href="#" class="scroll-top btn-hover">
+   <i class="lni lni-chevron-up"></i>
+   </a>
+</template>
+<script>
+export default {
+  //
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//import './assets/main.css'
+import './assets/vuetemplate/public/css/animate.css'
+import './assets/vuetemplate/public/css/main.css'
+//import './assets/vuetemplate/public/css/Linelcons.2.0.css'
+//import './assets/vuetemplate/public/css/bootstrap-5.0.0-alpha-2.min.css'
+createApp(App).mount('#app')
+
+
